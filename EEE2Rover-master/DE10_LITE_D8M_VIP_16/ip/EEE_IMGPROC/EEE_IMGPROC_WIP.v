cb96292a6a99f74cbedae54345a05694e9db5997
emtekcan <105928904+emtekcan@users.noreply.github.com>
2023-05-29 23:38:05 +0100

    RLE Compression 2.0

--- EEE2Rover-master/DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC_WIP.v
+++ EEE2Rover-master/DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC_WIP.v
@@ -65,10 +65,9 @@ input                         mode;
 //
 parameter IMAGE_W = 11'd640;
 parameter IMAGE_H = 11'd480;
-parameter POV_H = 8'd254;
-parameter POV_W = 10'd640;
 parameter MESSAGE_BUF_MAX = 256;
 parameter MSG_INTERVAL = 6;
+parameter BB_COL_DEFAULT = 24'h00ff00;
 
 
 wire [7:0]   red, green, blue, grey;
@@ -79,12 +78,20 @@ wire         sop, eop, in_valid, out_ready;
 
 // Detect white areas (illuminated path)
 wire white_detect;
-assign white_detect = red[7] & green[7] & blue[7] & y > 11'd254; //potential for further efficiency here by comparison with 8'd254 instead of 11'd254?
+assign white_detect = red[7] & green[7] & blue[7] & y > 11'd240;
+
+// Find boundary of cursor box
 
 // Highlight detected areas
 wire [23:0] white_high;
 assign grey = green[7:1] + red[7:2] + blue[7:2]; //Grey = green/2 + red/4 + blue/4
 assign white_high  =  white_detect ? {8'hff, 8'hff, 8'hff} : {grey, grey, grey};
+
+// Show bounding box
+wire [23:0] new_image;
+wire bb_active;
+assign bb_active = (x == left) | (x == right) | (y == top) | (y == bottom);
+assign new_image = bb_active ? bb_col : white_high;
 
 // Switch output pixels depending on mode switch
 // Don't modify the start-of-packet word - it's a packet discriptor
@@ -111,50 +118,101 @@ always@(posedge clk) begin
 	end
 end
 
+
 //White Chunk Compression Algorithm (RLE Encoding)
-reg [9:0] x_start, chunk_length;
+reg [9:0] x_start;
+reg [12:0] chunk_length;
 reg [7:0] y_start;
 reg in_chunk, send_chunk; //to keep track if current pixel under evaluation at current clk posedge is within chunk
+reg [31:0] chunk_buffer [255:0];
+reg [7:0] chunk_buffer_occupancy; //tells nios how many chunks to read from the buffer to obtain one frame
+reg sample;
+
+//frame counter
+reg [2:0] frame_counter;
+initial frame_counter = 3'd0;
+always@(posedge clk) begin
+	if (eop & in_valid & packet_video) begin  //Ignore non-video packets
+		frame_counter <= frame_counter + 1;
+		
+		if (frame_counter == 3'd5) begin
+			sample <= 1'b1;
+			frame_counter <= 3'd0;
+			chunk_buffer_occupancy <= 8'b0;
+			in_chunk <= 1'b0;
+		end
+		else sample <= 1'b0;
+	end
+end
 
 //reg new_frame; set equal to SOP when writing to buffer
 
 always@(posedge clk) begin
-	if(white_detect & (!in_chunk) & in_valid) begin 
-		x_start <= x;
-		y_start <= y;
-		in_chunk <= 1;
-		chunk_length <= 1;
-		send_chunk <= 0;
-	end
-	else if(white_detect & in_chunk & in_valid) begin
-		chunk_length <= chunk_length + 1;
-	end
-	else if((!white_detect) & in_chunk & in_valid) begin
-		in_chunk <= 0;
-		send_chunk <= 1;
-	end
-	if(sop & in_valid) begin
-		x_start <= 0;
-		y_start <= 0;
-		in_chunk <= 0;
-		chunk_length <= 0;
-		send_chunk <= 0;
+	if(sample) begin
+		if(white_detect & (!in_chunk) & in_valid) begin 
+			x_start <= x;
+			y_start <= y;
+			in_chunk <= 1'b1;
+			chunk_length <= 13'h1;
+		end
+		else if(white_detect & in_chunk & in_valid) begin
+			chunk_length <= chunk_length + 13'h1;
+		end
+		else if((!white_detect) & in_chunk & in_valid) begin
+			in_chunk <= 0;
+			if (chunk_buffer_occupancy == 8'h0) begin
+				chunk_buffer[chunk_buffer_occupancy] <= {1'b1, x_start, y_start, chunk_length};
+			end
+			else begin
+				chunk_buffer[chunk_buffer_occupancy] <= {1'b0, x_start, y_start, chunk_length};
+			end
+			chunk_buffer_occupancy <= chunk_buffer_occupancy + 8'h1;
+		end
 	end
 end
 
-// Adjusted FSM to handle sending chunks to NIOS, no need to maintain frame count anymore I hope
-reg msg_state;
-
+//Find first and last white pixels
+reg [10:0] x_min, y_min, x_max, y_max;
 always@(posedge clk) begin
-	if (eop & in_valid & packet_video & send_chunk) begin  //Ignore non-video packets		
-		//Start message writer FSM if there is room in the FIFO
-		if (msg_buf_size < MESSAGE_BUF_MAX - 1) begin
-			msg_state <= 1'b1;
+	if (white_detect & in_valid) begin	//Update bounds when the pixel is white
+		if (x < x_min) x_min <= x;
+		if (x > x_max) x_max <= x;
+		if (y < y_min) y_min <= y;
+		y_max <= y;
+	end
+	if (sop & in_valid) begin	//Reset bounds on start of packet
+		x_min <= IMAGE_W-11'h1;
+		x_max <= 0;
+		y_min <= IMAGE_H-11'h1;
+		y_max <= 0;
+	end
+end
+
+//Process bounding box at the end of the frame.
+reg [1:0] msg_state;
+reg [10:0] left, right, top, bottom;
+reg [7:0] frame_count;
+always@(posedge clk) begin
+	if (eop & in_valid & packet_video) begin  //Ignore non-video packets
+		
+		//Latch edges for display overlay on next frame
+		left <= x_min;
+		right <= x_max;
+		top <= y_min;
+		bottom <= y_max;
+		
+		
+		//Start message writer FSM once every MSG_INTERVAL frames, if there is room in the FIFO
+		frame_count <= frame_count - 1;
+		
+		if (frame_count == 0 && msg_buf_size < MESSAGE_BUF_MAX - 3) begin
+			msg_state <= 2'b01;
+			frame_count <= MSG_INTERVAL-1;
 		end
 	end
 	
 	//Cycle through message writer states once started
-	if (!msg_state) msg_state <= 0;
+	if (msg_state != 2'b00) msg_state <= msg_state + 2'b01;
 
 end
 	
@@ -166,15 +224,24 @@ wire msg_buf_rd, msg_buf_flush;
 wire [7:0] msg_buf_size;
 wire msg_buf_empty;
 
+`define RED_BOX_MSG_ID "RBB"
+
 always@(*) begin	//Write words to FIFO as state machine advances
 	case(msg_state)
-		1'b0: begin
+		2'b00: begin
 			msg_buf_in = 32'b0;
 			msg_buf_wr = 1'b0;
 		end
-		1'b1: begin
-			msg_buf_in = {3'b0, x_start, y_start, chunk_length, sop};   // 0padding | x_start | y_start | chunk_len | sop |
-																		//  [31:29] | [28:19] | [18:11] |  [10:1]   | [0] |
+		2'b01: begin
+			msg_buf_in = `RED_BOX_MSG_ID;	//Message ID
+			msg_buf_wr = 1'b1;
+		end
+		2'b10: begin
+			msg_buf_in = {5'b0, x_min, 5'b0, y_min};	//Top left coordinate
+			msg_buf_wr = 1'b1;
+		end
+		2'b11: begin
+			msg_buf_in = {5'b0, x_max, 5'b0, y_max}; //Bottom right coordinate
 			msg_buf_wr = 1'b1;
 		end
 	endcase
