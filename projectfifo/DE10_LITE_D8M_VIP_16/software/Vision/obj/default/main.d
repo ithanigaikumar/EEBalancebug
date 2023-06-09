f20e9823e4b12accb2d1cc5b0078f8df0b7188f6
emtekcan <105928904+emtekcan@users.noreply.github.com>
2023-06-09 16:22:00 +0100

    updated code

---
+++ projectfifo/DE10_LITE_D8M_VIP_16/software/Vision/obj/default/main.d
@@ -0,0 +1,84 @@
+obj/default/main.o: main.c I2C_core.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/alt_types.h \
+ terasic_includes.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash_types.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash_dev.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_llist.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/priv/alt_dev_llist.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_cache.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash_types.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/io.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/alt_types.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/drivers/inc/altera_avalon_pio_regs.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_irq.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/nios2.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/system.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/linker.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_alarm.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_llist.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/priv/alt_alarm.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_timestamp.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/drivers/inc/altera_avalon_timer.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_dev.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_warning.h \
+ C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_stdio.h \
+ system.h mipi_camera_config.h mipi_bridge_config.h auto_focus.h
+
+I2C_core.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/alt_types.h:
+
+terasic_includes.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash_types.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash_dev.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_llist.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/priv/alt_dev_llist.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_cache.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_flash_types.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/io.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/alt_types.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/drivers/inc/altera_avalon_pio_regs.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_irq.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/nios2.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/system.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/linker.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_alarm.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_llist.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/priv/alt_alarm.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_timestamp.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/drivers/inc/altera_avalon_timer.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_dev.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_warning.h:
+
+C:/Users/Emre/Documents/projectnios2/DE10_LITE_D8M_VIP_16/software/Vision_bsp/HAL/inc/sys/alt_stdio.h:
+
+system.h:
+
+mipi_camera_config.h:
+
+mipi_bridge_config.h:
+
+auto_focus.h:
