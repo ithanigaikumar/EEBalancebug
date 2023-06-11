9cf21dd45ad8d3ad83ecfb4149c46533f133b7a6
DamaniMC <84398982+DamaniMC@users.noreply.github.com>
2023-06-11 17:49:57 +0100

    idk

---
+++ EEE2Rover-master/src/server.py
@@ -0,0 +1,25 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/your_file.html'
+     
+
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
